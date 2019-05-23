82092f9220cb17cfa4d98f0161942f19934743ff
LiQixiang <791902694@qq.com>
2019-05-23 19:58:54 +0800

    dasd

---
+++ src/com/neuedu/homeword/work0523.java
@@ -0,0 +1,13 @@
+package com.neuedu.homeword;
+
+public class work0523 {
+    public static void main(String[] args) {
+        int a=705;
+        int b=(a%10);
+        int c=(a%100-b)/10;
+        int d=(a%1000-b-c*10)/100;
+        System.out.println(b*100+c*10+d);
+
+
+    }
+}
