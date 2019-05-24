69c82a755fc4c0d8b54850007b5bbf8ccb5932d8
LiQixiang <791902694@qq.com>
2019-05-24 16:16:55 +0800

    0523作业

--- src/com/neuedu/homeword/work0523.java
+++ src/com/neuedu/homeword/work0523.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 
 public class work0523 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int a = input.nextInt();
+        int a=705;
         int b=(a%10);
         int c=(a%100-b)/10;
         int d=(a%1000-b-c*10)/100;
