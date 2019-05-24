e3f895203473c868d246588bc828bcbb6d62f976
LiQixiang <791902694@qq.com>
2019-05-24 16:11:11 +0800

    dasd

--- src/com/neuedu/homeword/work0523.java
+++ src/com/neuedu/homeword/work0523.java
@@ -1,13 +1,14 @@
 package com.neuedu.homeword;
 
+import java.util.Scanner;
+
 public class work0523 {
     public static void main(String[] args) {
-        int a=705;
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
         int b=(a%10);
         int c=(a%100-b)/10;
         int d=(a%1000-b-c*10)/100;
         System.out.println(b*100+c*10+d);
-
-
     }
 }
