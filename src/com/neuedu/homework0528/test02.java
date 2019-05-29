cdcb68d2d4b3f30c0b5d77a19cb3a2356139d475
LiQixiang <791902694@qq.com>
2019-05-29 09:02:18 +0800

    0528作业

--- src/com/neuedu/homework0528/test02.java
+++ src/com/neuedu/homework0528/test02.java
@@ -2,6 +2,10 @@ package com.neuedu.homework0528;
 
 public class test02 {
     public static void main(String[] args) {
+                  work02 ben1 =new work02();
+                     System.out.println(ben1);
+                  work02 ben2=new work02('绿',56);
+                     System.out.println(ben2);
 
     }
 }
--- src/com/neuedu/homework0528/work02.java
+++ src/com/neuedu/homework0528/work02.java
@@ -4,13 +4,21 @@ public class work02 {
     //2、定义一个笔记本类，该类有颜色（char）和cpu型号（int）两个属性。
     //2.1 无参和有参的两个构造方法；有参构造方法可以在创建对象的同时为每个属性赋值；
     //2.2 输出笔记本信息的方法 3.3 然后编写一个测试类，测试笔记本类的各个方法。
-    char color;
-    int cpu;
+    private char color;
+    private int cpu;
 
-    public void wucan(){
+    public work02(){
+          color='红';
+          cpu=45;
+    }
+    public  work02(char color,int cpu){
+           this.color=color;
+           this.cpu=cpu;
 
     }
-    public void youcan(char color1,int cpu1){
 
+    @Override
+    public String toString() {
+        return "颜色为："+color+" 型号为"+cpu;
     }
 }
